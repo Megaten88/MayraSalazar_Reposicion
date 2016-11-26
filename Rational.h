53365aef7769277efb1324f773a0d8eb0ff23c1e
Mayra Salazar <mayrasalazar@unitec.edu>
2016-11-26 14:11:37 -0600

    Reposición de Lab

---
+++ Rational.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+using std::string;
+class Rational
+{
+private:
+	int numerator;
+	int denominator;
+	int maxDivisor(int,int);
+	void setNumerator(int);
+	void setDenominator(int);
+public:
+	Rational(int, int);
+	string toString();
+	~Rational();
+	int getNumerator();
+	int getDenominator();
+	string operator+(Rational&);
+	string operator-(Rational&);
+	string operator*(Rational&);
+	string operator/(Rational&);	
+};
